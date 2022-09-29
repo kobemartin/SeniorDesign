6629d3a4d4274cad474a2123eabd5ddeec87045f
kobemartin <kobe.martin1@gmail.com>
2022-09-29 14:36:49 -0400

    Updated main.ino to blink LED from MQTT topic

--- IOTCore/main/main.ino
+++ IOTCore/main/main.ino
@@ -1,83 +1,105 @@
 #include "secrets.h"
 #include <WiFiClientSecure.h>
-#include <MQTTClient.h>
+#include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
-
-// The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+ 
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
-
+ 
+#define lamp 23
+ 
 WiFiClientSecure net = WiFiClientSecure();
-MQTTClient client = MQTTClient(256);
-
+PubSubClient client(net);
+ 
+ 
 void connectAWS()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
+ 
   Serial.println("Connecting to Wi-Fi");
-
-  while (WiFi.status() != WL_CONNECTED){
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-
+ 
   // Configure WiFiClientSecure to use the AWS IoT device credentials
   net.setCACert(AWS_CERT_CA);
   net.setCertificate(AWS_CERT_CRT);
   net.setPrivateKey(AWS_CERT_PRIVATE);
-
+ 
   // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.begin(AWS_IOT_ENDPOINT, 8883, net);
-
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
   // Create a message handler
-  client.onMessage(messageHandler);
-
-  Serial.print("Connecting to AWS IOT");
-
-  while (!client.connect(THINGNAME)) {
+  client.setCallback(messageHandler);
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
     Serial.print(".");
     delay(100);
   }
-
-  if(!client.connected()){
+ 
+  if (!client.connected())
+  {
     Serial.println("AWS IoT Timeout!");
     return;
   }
-
+ 
   // Subscribe to a topic
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-
+ 
   Serial.println("AWS IoT Connected!");
 }
-
-void publishMessage()
+ 
+ 
+void messageHandler(char* topic, byte* payload, unsigned int length)
 {
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
   StaticJsonDocument<200> doc;
-  doc["time"] = millis();
-  doc["sensor_a0"] = analogRead(0);
-  char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // print to client
-
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+   Serial.println();
+ 
+  for (int i = 0; i < length; i++) 
+  {
+    Serial.print((char)payload[i]); // Pring payload content
+  }
+    char led = (char)payload[62]; // Extracting the controlling command from the Payload to Controlling LED from AWS
+    Serial.print("Command: ");
+    Serial.println(led);
+ 
+  if (led == 49) // 49 is the ASCI value of 1
+  {
+    digitalWrite(lamp, HIGH);
+    Serial.println("Lamp_State changed to HIGH");
+  }
+  else if (led == 48) // 48 is the ASCI value of 0
+  {
+    digitalWrite(lamp, LOW);
+    Serial.println("Lamp_State changed to LOW");
+  }
+  Serial.println();
 }
-
-void messageHandler(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-//  StaticJsonDocument<200> doc;
-//  deserializeJson(doc, payload);
-//  const char* message = doc["message"];
-}
-
-void setup() {
-  Serial.begin(9600);
+ 
+ 
+void setup()
+{
+  Serial.begin(115200);
   connectAWS();
+  pinMode (lamp, OUTPUT);
+  digitalWrite(lamp, LOW);
+ 
 }
-
-void loop() {
-  publishMessage();
+ 
+void loop()
+{
   client.loop();
   delay(1000);
 }
